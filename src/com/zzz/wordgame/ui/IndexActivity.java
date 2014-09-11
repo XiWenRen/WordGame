b09988c044e66ccea6354fbb78245c78f9750616
Jing <zyj5555@qq.com>
2014-09-11 18:12:40 +0800

    测试

--- src/com/zzz/wordgame/ui/IndexActivity.java
+++ src/com/zzz/wordgame/ui/IndexActivity.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
  */
 public class IndexActivity extends BaseActivity{
 	
-	@Override
+	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_index);
